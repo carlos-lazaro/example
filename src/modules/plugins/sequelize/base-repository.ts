23d47091b3015548960fe75d40892a7b379943fb
Carlos Alfonso Lazaro Ferrer <carallafe@gmail.com>
2023-07-26 15:05:43 -0500

    feat: middlewares, awilux and some base component

--- src/modules/plugins/sequelize/base-repository.ts
+++ src/modules/plugins/sequelize/base-repository.ts
@@ -1,10 +1,7 @@
-import { DataTypes, Model, ModelStatic } from "sequelize";
-import sequelize, { Sequelize } from "sequelize/types/sequelize";
+import { Model, ModelStatic } from "sequelize";
 
 import { User } from "../../../domain";
 import { Repository } from "../../../interfaces/database/interface/base-repository";
-import { container } from "../../dependency-injection";
-import { SequelizeDatabase } from "./sequelize-database";
 
 export abstract class BaseRepository<T extends Model<User, any>>
   implements Repository<T>
@@ -24,9 +21,8 @@ export abstract class BaseRepository<T extends Model<User, any>>
   }
 
   async create(entity: T): Promise<T> {
-    return this.model.create(entity);
+    throw new Error("Method not implemented.");
   }
-
   // async create(entity: Partial<T>): Promise<T> {
   //   return this.model.create(entity);
   // }
@@ -39,18 +35,26 @@ export abstract class BaseRepository<T extends Model<User, any>>
   //   return this.model.create(entity);
   // }
 
-  async update(id: number, entity: Partial<T>): Promise<T | null> {
-    const foundEntity = await this.model.findByPk(id);
-    if (foundEntity) {
-      return foundEntity.update(entity);
-    }
-    return null;
+  async update(id: number, entity: T): Promise<T | null> {
+    throw new Error("Method not implemented.");
   }
 
+  // async update(id: number, entity: Partial<T>): Promise<T | null> {
+  //   const foundEntity = await this.model.findByPk(id);
+  //   if (foundEntity) {
+  //     return foundEntity.update(entity);
+  //   }
+  //   return null;
+  // }
+
   async delete(id: number): Promise<boolean> {
-    const deletedRowCount = await this.model.destroy({ where: { id } });
-    return deletedRowCount > 0;
+    throw new Error("Method not implemented.");
   }
+
+  // async delete(id: number): Promise<boolean> {
+  //   const deletedRowCount = await this.model.destroy({ where: { id } });
+  //   return deletedRowCount > 0;
+  // }
 
   async paginate(
     page: number,
